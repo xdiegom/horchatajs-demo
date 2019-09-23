358b87edbe976259d365a97dd6601b33ab535989
Diego Melgar <diego.melgar.sv@gmail.com>
2019-09-22 19:55:08 -0600

    :lipstick: Changed body text-color

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
           <nuxt-link
             :class="{'border-b-2': post.id < posts.length}"
             :to="{ name: 'posts-id', params: { id: post.id } }"
-            class="text-xs text-gray-600 font-bold p-4 hover:text-black md:text-base">
+            class="text-xs font-bold p-4 hover:text-nuxt-light md:text-base">
             {{ post.title }}
           </nuxt-link>
         </div>
